221b497d1d9b040457e277ebc1fbe9fa5f332899
walkingpendulum <oayunin@gmail.com>
2021-06-27 11:31:56 +0300

    waiter function for clicking already located element default waiters works only if you want discover element first and then click, but we have alread discovered element previously.

--- browser_utils.py
+++ browser_utils.py
@@ -1,8 +1,11 @@
+import time
 from contextlib import contextmanager
 
 from selenium import webdriver
+from selenium.common.exceptions import ElementNotInteractableException
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
 
 @contextmanager
@@ -18,3 +21,14 @@ def make_driver(headless: bool = True, cleanup: bool = True) -> WebDriver:
     finally:
         if cleanup:
             driver.quit()
+
+
+def click_until_clicks(element: WebElement, timeout: int = 10) -> WebElement:
+    start_time = time.time()
+    while time.time() - start_time < timeout:
+        try:
+            return element.click()
+        except ElementNotInteractableException:
+            time.sleep(0.5)
+
+    return element.click()
